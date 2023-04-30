1d6ce793d40466f125049d6f972ee3e8bfc135fa
Maksim <InDigitalE8@gmail.com>
2023-04-30 23:05:06 +0400

    Hotfix

--- run.py
+++ run.py
@@ -1,14 +1,20 @@
 import logging
+from pathlib import Path
 
 from app import start_bot
 from utils import get_root_dir
 
 
 def init_logger() -> None:
+    # Create /logs dir if not exist
+    log_dir = get_root_dir() + '/logs'
+    Path(log_dir).mkdir(parents=True, exist_ok=True)
+
+    # Init Logger
     logger = logging.getLogger()
     formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
 
-    file_handler = logging.FileHandler(f"{get_root_dir()}/logs/debug.log", mode='a')
+    file_handler = logging.FileHandler(log_dir + '/debug.log', 'a')
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
 
